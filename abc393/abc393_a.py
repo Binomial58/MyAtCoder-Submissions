3877edd8eee20674abee6d9f9edc169cb8fc0784
AutoCloneBot <acautoclone@bot.com>
2025-02-25 14:51:53 +0000

    Added my codes from AtCoder

---
+++ abc393/abc393_a.py
@@ -0,0 +1,11 @@
+A,B=map(str, input().split())
+if A=="sick":
+    if B=="sick":
+        print(1)
+    else:
+        print(2)
+else:
+    if B=="sick":
+        print(3)
+    else:
+        print(4)
