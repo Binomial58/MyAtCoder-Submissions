719a685f51809172f208e48486dcf4022c5527ca
AutoCloneBot <acautoclone@bot.com>
2025-03-06 14:51:26 +0000

    Added my codes from AtCoder

---
+++ abc255/abc255_a.py
@@ -0,0 +1,5 @@
+R,C=map(int, input().split())
+A=list(map(int, input().split()))
+B=list(map(int, input().split()))
+D=[A,B]
+print(D[R-1][C-1])
---
+++ abc267/abc267_a.py
@@ -0,0 +1,3 @@
+S=input()
+T=["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+print(5-T.index(S))
